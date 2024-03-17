f3d87243aa06b835a4dd91e78c21424dff98a027
Jesse Gibson <JesseTheGibson@gmail.com>
2024-03-17 16:48:52 -0400

    Add tests/clippy
    I'm experimenting with stricter Clippy configurations. We'll see if it
    sticks.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![warn(clippy::print_stdout, clippy::print_stderr)]
+#![deny(
+    clippy::pedantic,
+    clippy::perf,
+    clippy::style,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::suspicious,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::self_named_module_files,
+    clippy::shadow_reuse
+)]
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {}
+
 fn main() {
-    println!("Hello, world!");
+    Args::parse();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use clap::CommandFactory;
+
+    #[test]
+    fn test_basic_command_parsing() {
+        // Simply test that it does not crash.
+        Args::command().get_matches_from(vec![""]);
+    }
 }
